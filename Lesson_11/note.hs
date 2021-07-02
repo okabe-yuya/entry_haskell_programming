dbff8465752a19caedf6b989decc441e87ee8ed9
okabe-yuya <takamizawa46@gmail.com>
2021-07-03 07:54:03 +0900

    fix memo

--- Lesson_11/note.hs
+++ Lesson_11/note.hs
@@ -3,3 +3,9 @@ halve a b = div a b
 
 printDouble :: Int -> String 
 printDouble n = show (n * 2)
+
+-- MEMO: headでは空のリストを返すことが出来ない
+-- 現状だと戻り値の方を統一する必要があるため
+-- emptyHead :: [a] -> [b]
+-- emptyHead [] = []
+-- emptyHead (x:_) = x
